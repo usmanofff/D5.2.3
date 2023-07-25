e176199daffe754f6507b6388316170ff90990be
ubuntu <usmanofff@gmail.com>
2023-07-25 17:09:44 +1000

    change pushgateway

--- deploy-helm.sh
+++ deploy-helm.sh
@@ -3,7 +3,7 @@ git clone git@github.com:SkillfactoryCoding/DEVOPS-Helm-helm-charts.git
 
 cd DEVOPS-Helm-helm-charts/charts/prometheus && helm dependency build && helm -n prometheus install prometheus-apps .
 
-helm -n prometheus upgrade prometheus-apps . --set alertmanager.enabled=false, --set pushgetway.enabled=false, --set nodeExporter.enabled=false
+helm -n prometheus upgrade prometheus-apps . --set alertmanager.enabled=false, --set pushgateway.enabled=false, --set nodeExporter.enabled=false
 
 count=1
 while  [ $count -le 10 ]
