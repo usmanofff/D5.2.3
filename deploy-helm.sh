fec3b17eeb3d1c4d4fcdd8b234c273484c8d500c
ubuntu <usmanofff@gmail.com>
2023-07-25 16:57:19 +1000

    prometheus

---
+++ deploy-helm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git clone git@github.com:SkillfactoryCoding/DEVOPS-Helm-helm-charts.git
+
+cd DEVOPS-Helm-helm-charts/charts/prometheus && helm dependency build && helm -n prometheus install prometheus-apps .
+
+helm -n prometheus upgrade prometheus-apps . --set alertmanager.enabled=false, --set pushgetway.enabled=false, --set nodeExporter.enabled=false
+
+count=1
+while  [ $count -le 10 ]
+do
+kubectl -n prometheus get po
+  echo $count
+ sleep 1
+  count=$(( $count + 1 ))
+done
+
+helm -n prometheus uninstall prometheus-apps 
+
